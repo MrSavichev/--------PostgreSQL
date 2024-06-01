815f72a3fb2f8d87c54cf6e58050db08cee662d4
Dmitry <mistersavitschev@gmail.com>
2024-06-02 01:30:09 +0300

    update scrypt

--- insert_rows.py
+++ insert_rows.py
@@ -1,11 +1,14 @@
 import psycopg2  # Импортируем библиотеку, которая позволяет нам работать с PostgreSQL в Python
 import time  # Импортируем библиотеку для работы со временем
+import logging # Импортируем библиотеку для логирования
+
+logging.basicConfig(level=logging.INFO) # Настраиваем логирование
 
 def insert_rows():
     conn = None  # Инициализируем переменную для подключения к базе данных как None
     while True:  # Начинаем бесконечный цикл
         try:
-            # Используем IP-адрес контейнера HAProxy
+            # Подключаемся к HAProxy по порту 5000
             conn = psycopg2.connect(
                 host="172.20.0.2",  # Подключаемся через имя контейнера HAProxy
                 port=5000,  # Указываем порт HAProxy
@@ -19,17 +22,21 @@ def insert_rows():
             cur.execute("CREATE TABLE IF NOT EXISTS test_table (id SERIAL PRIMARY KEY, timestamp TIMESTAMPTZ DEFAULT NOW())")
             # Вставляем новую строку в таблицу
             cur.execute("INSERT INTO test_table DEFAULT VALUES")
-            conn.commit()  # Применяем изменения в базе данных
-
-            cur.close()  # Закрываем курсор
-            conn.close()  # Закрываем соединение с базой данных
-            print("Inserted a row.")  # Выводим сообщение о вставке строки
-            time.sleep(5)  # Приостанавливаем выполнение на 5 секунд
-        except Exception as error:  # Обрабатываем возникшие исключения
-            print(f"Error: {error}")  # Выводим сообщение об ошибке
-            if conn:  # Если соединение было установлено
-                conn.close()  # Закрываем соединение
-            time.sleep(5)  # Приостанавливаем выполнение на 5 секунд в случае ошибки
+            conn.commit() # Сохраняем изменения
+            cur.close() # Закрываем курсор
+            conn.close() # Закрываем подключение
+            logging.info("Inserted a row.") # Выводим сообщение в лог
+            time.sleep(5) # Пауза 5 секунд
+        except psycopg2.OperationalError as e: # Обрабатываем ошибки
+            logging.error(f"Operational error: {e}") # Выводим сообщение в лог
+            if conn: # Если подключение существует
+                conn.close() # Закрываем подключение
+            time.sleep(5) # Пауза 5 секунд
+        except Exception as e: # Обрабатываем остальные ошибки
+            logging.error(f"Unexpected error: {e}") # Выводим сообщение в лог
+            if conn: # Если подключение существует
+                conn.close() # Закрываем подключение
+            time.sleep(5) # Пауза 5 секунд
 
 if __name__ == "__main__":
     insert_rows()  # Запускаем функцию insert_rows
